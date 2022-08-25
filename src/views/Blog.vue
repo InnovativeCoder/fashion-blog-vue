f299f555e8bf05fb08f425a4682a02466be55ec4
InnovativeCoder <aashitsingh2010@gmail.com>
2022-08-24 21:55:30 -0400

    added a router

---
+++ src/views/Blog.vue
@@ -0,0 +1,30 @@
+<template>
+     <div class="main">
+          <h1>
+               Blog Title
+          </h1>
+          <section class="blog-body">
+               content of blog {{$route.params.id}}
+               {{$data}}
+          </section>
+     </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+import { useRoute, useRouter } from 'vue-router';
+import data from "../data/blog-data.json";
+
+export default defineComponent({
+     setup() {
+          const router = useRouter()
+          const route = useRoute()
+          
+          return(data)
+     },
+})
+</script>
+<style>
+     .blog-body{
+          color:rgb(100, 100, 112)
+     }
+</style>
